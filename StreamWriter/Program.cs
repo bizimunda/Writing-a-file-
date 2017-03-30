06363e87d78dd461066a7bf151f4610939fa60a2
bizimunda <yourbelover@gmail.com>
2017-03-30 13:52:42 +0200

    initial

---
+++ StreamWriter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamWriterr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the directories currently on the C drive.
+            DirectoryInfo[] cDirs = new DirectoryInfo(@"c:\").GetDirectories();
+
+            // Write each directory name to a file.
+            using (StreamWriter sw = new StreamWriter("CDriveDirs.txt"))
+            {
+                foreach (DirectoryInfo dir in cDirs)
+                {
+                    sw.WriteLine(dir.Name);
+                }
+            }
+            
+            string line = "";
+            using (StreamReader sr = new StreamReader("CDriveDirs.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    int total=line.Length;
+                    Console.WriteLine(total);
+                    Console.WriteLine(line);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
